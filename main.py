aa7a8d165641d7d285c5250e1170ebd1ba1c2ad7
yarden <yarden.tulchinsky@gmail.com>
2024-06-20 18:12:39 +0300

    getting from csv the words with pandas

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
-
-
+#---Get a new random word when buttons are pressed
+#---Getting the words from CVS file---
+words_dataframe = pandas.read_csv('./data/french_words.csv')
+words_dic = [{'English':row.English,'French':row.French} for (index,row) in words_dataframe.iterrows()]
+random_word = random.choice(words_dic)
+english_word = random_word['English']
+french_word = random_word['French']
 #----UI----
 window = Tk()
 window.title('Flash Cards')
@@ -21,8 +28,8 @@ wrong_btn.grid(row=1, column=0)
 canvas = Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 front_card_image = PhotoImage(file='./images/card_front.png')
 canvas.create_image(400,263,image=front_card_image)
-canvas.create_text(400,150,text='English', font=('Ariel',40,'italic'))
-canvas.create_text(400,263, text='Word', font=('Ariel',60,'bold'))
+canvas.create_text(400,150,text=f'French', font=('Ariel',40,'italic'))
+canvas.create_text(400,263, text=f'{french_word}', font=('Ariel',60,'bold'))
 canvas.grid(row=0,column=0,columnspan=2)
 
 window.mainloop()
