ab2f363b28db189230cd62c789bfa18f080b2c10
yarden <yarden.tulchinsky@gmail.com>
2024-06-21 08:45:08 +0300

    adding function for changing card, including changing image and the text after 3 seconds and change it back to french after clicking the buttons

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
+current_english = ""
+current_french =""
+
 
 #---Getting the words from CVS file---
 words_dataframe = pandas.read_csv('./data/french_words.csv')
-words_dic = [{'English':row.English,'French':row.French} for (index,row) in words_dataframe.iterrows()]
-random_word = random.choice(words_dic)
-english_word = random_word['English']
-french_word = random_word['French']
+words_dic = words_dataframe.to_dict(orient="records")
+
+#----Function for changing the word from french to english
+def change_language():
+    canvas.itemconfig(language_text, text='English',fill='white')
+    canvas.itemconfig(french_text, text=f'{current_english}',fill='white')
+    canvas.itemconfig(card_canvas,image=back_card_image)
+
 
 #---Get a new random word when buttons are pressed---
-def random_func():
-    random_word = random.choice(words_dic)
-    english_word = random_word['English']
-    french_word = random_word['French']
-    canvas.itemconfig(french_text,text=f'{french_word}')
+def random_word_pick():
+    global current_french
+    global current_english
+    current_card = random.choice(words_dic)
+    current_english = current_card['English']
+    current_french = current_card['French']
+    canvas.itemconfig(language_text,text='French',fill='black')
+    canvas.itemconfig(french_text,text=f'{current_french}',fill='black')
+    canvas.itemconfig(card_canvas, image=front_card_image)
+    window.after(3000, change_language)
+
 
 #----UI----
 window = Tk()
@@ -24,20 +37,24 @@ window.config(bg=BACKGROUND_COLOR, padx=50,pady=50)
 
 #implementing V button with image
 right_image = PhotoImage(file='./images/right.png')
-right_btn = Button(image=right_image, highlightthickness=0,command=random_func)
+right_btn = Button(image=right_image, highlightthickness=0,command=random_word_pick)
 right_btn.grid(row=1,column=1)
 
 #implementing X button with image
 wrong_image = PhotoImage(file='./images/wrong.png')
-wrong_btn = Button(image=wrong_image, highlightthickness=0,command=random_func)
+wrong_btn = Button(image=wrong_image, highlightthickness=0,command=random_word_pick)
 wrong_btn.grid(row=1, column=0)
 
 #creating canvas with card image and texts for words
 canvas = Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 front_card_image = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400,263,image=front_card_image)
-canvas.create_text(400,150,text=f'French', font=('Ariel',40,'italic'))
-french_text = canvas.create_text(400,263, text=f'{french_word}', font=('Ariel',60,'bold'))
+back_card_image = PhotoImage(file='./images/card_back.png')
+card_canvas = canvas.create_image(400,263,image=front_card_image)
+language_text = canvas.create_text(400,150,text='', font=('Ariel',40,'italic'))
+french_text = canvas.create_text(400,263, text='', font=('Ariel',60,'bold'))
 canvas.grid(row=0,column=0,columnspan=2)
+
+random_word_pick()
+
 
 window.mainloop()
